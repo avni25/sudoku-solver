10d4808671a5f651009d27aff278191fa520d72c
H. Avni Cubukcu <huseyinac@gmail.com>
2022-02-25 08:21:28 +0300

    post req

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const puzzleBoard = document.querySelector("#puzzle-board");
-const solveButton = document.querySelector(".solve-btn");
+const solveButton = document.querySelector("#solve-btn");
 const squares = 81;
 const given_numbers=[];
 
@@ -25,4 +25,25 @@ const getVals =() =>{
     console.log(given_numbers);
 }
 
-solveButton.addEventListener("click", getvals);
+const solve = ()=>{
+    var options = {
+        method: 'POST',
+        url: 'https://sudoku-solver2.p.rapidapi.com/',
+        headers: {
+          'content-type': 'application/json',
+          'x-rapidapi-host': 'sudoku-solver2.p.rapidapi.com',
+          'x-rapidapi-key': '9ff48dee7bmsh03eb472004792f3p14a609jsn3c96cd0b0524'
+        },
+        data: {
+          input: '53x2x7x4x6x2x195x4x98x4x6x1x8x3x6x3x34x2x8x1x3x2x17x3x2x3x6x1x6x4x28x4x419x2x5x4x8x2x79'
+        }
+      };
+      
+      axios.request(options).then(function (response) {
+          console.log(response.data);
+      }).catch(function (error) {
+          console.error(error);
+      });
+}
+
+solveButton.addEventListener("click", solve);
