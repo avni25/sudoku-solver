1919e01730a2ed6b20b615b77a928fc352ef5f3b
H. Avni Cubukcu <huseyinac@gmail.com>
2022-02-25 09:06:24 +0300

    ..

--- main.js
+++ main.js
@@ -24,21 +24,24 @@ const getVals =() =>{
         }
     });
     console.log(given_numbers);
+    var count=0;
     for(var i=0;i < given_numbers.length; i++){
-        var count=0;
-        if(given_numbers[i]=="x"){
-            var j=i;
-            while(given_numbers[j]=="x"){
-                count++;
-                j++;
-            }
-            i=j;
-            arr.push("x"+count+"x");
-        }else{
+        
+        if(given_numbers[i]!="x"){
+            if(count>0){
+                arr.push("x"+count+"x");
+            } 
             arr.push(given_numbers[i]);
+            count=0;
+        }else{
+            count++;
+        }
+        if(i==given_numbers.length-1){
+            arr.push("x"+count+"x");
         }
     }
     console.log(arr);
+    return arr.join("");
 }
 
 const solve = ()=>{
@@ -51,7 +54,8 @@ const solve = ()=>{
           'x-rapidapi-key': '9ff48dee7bmsh03eb472004792f3p14a609jsn3c96cd0b0524'
         },
         data: {
-          input: '53x2x7x4x6x2x195x4x98x4x6x1x8x3x6x3x34x2x8x1x3x2x17x3x2x3x6x1x6x4x28x4x419x2x5x4x8x2x79'
+          input: getVals()
+          //'53x2x7x4x6x2x195x4x98x4x6x1x8x3x6x3x34x2x8x1x3x2x17x3x2x3x6x1x6x4x28x4x419x2x5x4x8x2x79'
         }
       };
       
